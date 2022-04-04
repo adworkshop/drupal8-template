bb36f17ade4e08e3181066981f7c555abb66dd07
Jeff Cicero <jcicero@yourworkshop.com>
2022-04-04 12:58:14 -0400

    Drupal core 8.9.20

--- sites/example.settings.local.php
+++ sites/example.settings.local.php
@@ -1,4 +1,7 @@
 <?php
+
+// @codingStandardsIgnoreFile
+
 /**
  * @file
  * Local development override configuration feature.
@@ -52,6 +55,7 @@ $config['system.performance']['css']['preprocess'] = FALSE;
 $config['system.performance']['js']['preprocess'] = FALSE;
 
 /**
+ * Disable the render cache.
  *
  * Note: you should test with the render cache enabled, to ensure the correct
  * cacheability metadata is present. However, in the early stages of
@@ -60,8 +64,9 @@ $config['system.performance']['js']['preprocess'] = FALSE;
  * This setting disables the render cache by using the Null cache back-end
  * defined by the development.services.yml file above.
  *
+ * Only use this setting once the site has been installed.
  */
-$settings['cache']['bins']['render'] = 'cache.backend.null';
+# $settings['cache']['bins']['render'] = 'cache.backend.null';
 
 /**
  * Disable caching for migrations.
@@ -69,7 +74,7 @@ $settings['cache']['bins']['render'] = 'cache.backend.null';
  * Uncomment the code below to only store migrations in memory and not in the
  * database. This makes it easier to develop custom migrations.
  */
-$settings['cache']['bins']['discovery_migration'] = 'cache.backend.memory';
+# $settings['cache']['bins']['discovery_migration'] = 'cache.backend.memory';
 
 /**
  * Disable Internal Page Cache.
@@ -83,7 +88,7 @@ $settings['cache']['bins']['discovery_migration'] = 'cache.backend.memory';
  *
  * Only use this setting once the site has been installed.
  */
-$settings['cache']['bins']['page'] = 'cache.backend.null';
+# $settings['cache']['bins']['page'] = 'cache.backend.null';
 
 /**
  * Disable Dynamic Page Cache.
@@ -92,7 +97,7 @@ $settings['cache']['bins']['page'] = 'cache.backend.null';
  * cacheability metadata is present (and hence the expected behavior). However,
  * in the early stages of development, you may want to disable it.
  */
-$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+# $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
 
 /**
  * Allow test modules and themes to be installed.
@@ -101,7 +106,7 @@ $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
  * During development it can be useful to install test extensions for debugging
  * purposes.
  */
-$settings['extension_discovery_scan_tests'] = TRUE;
+# $settings['extension_discovery_scan_tests'] = TRUE;
 
 /**
  * Enable access to rebuild.php.
